44b42665dd84812d6b6817a01b027576d3e12193
Felipe Catalán <facatalan@gmail.com>
2011-05-17 17:58:11 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JardinesApp::Application.config.secret_token = '9380121f5fbea182d85a126869a165db62e1f06540804a8e0224a7f0c4894d54e0c9052dfdbeb4e25bf90cbb1567ed744edab3cc1e275e77693bde549f83df06'
